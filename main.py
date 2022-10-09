f12a682c1aa562d94017533d51d228f7b96c5e9e
Jake Peach <112898008+4b3j@users.noreply.github.com>
2022-10-09 09:59:36 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import keyboard
 snapchat_send_button = "images/send_snap.png"
 snapchat_new_snap_button = "images/new_snap_3.png"
 snapchat_reply = "images/reply_button_2.png"
+snapchat_back = "images/snap_go_back.png"
 
 reply_button_x = 930
 reply_button_y = 870
@@ -26,15 +27,19 @@ def send_snap(loc):
 
 
 while keyboard.is_pressed('F') == False:
-    time.sleep(1)
-
+    find_go_back_button = pyautogui.locateOnScreen(snapchat_back, grayscale=False, confidence=0.9)
     find_snap = pyautogui.locateOnScreen(snapchat_send_button, grayscale=False, confidence=0.7)
     find_new_snap = pyautogui.locateOnScreen(snapchat_new_snap_button, grayscale=False, confidence=0.9)
     find_reply_button = pyautogui.locateOnScreen(snapchat_reply, grayscale=False, confidence=0.5)
 
-    print(find_snap, find_new_snap, find_reply_button)
-
-    if find_snap != None:
+    # print(find_snap, find_new_snap, find_reply_button)
+    
+    
+    if find_go_back_button != None:
+        # This will check if its on a users chat and go off it
+        pyautogui.click(find_go_back_button)
+        continue
+    elif find_snap != None:
         replies+=1
         print(f"{replies} New Snap Detected ")
         continue
@@ -47,6 +52,6 @@ while keyboard.is_pressed('F') == False:
         replies+=1
         print(f"{replies} New Snap Detected")
         pyautogui.click(find_new_snap)
-        time.sleep(2)
+        time.sleep(1)
         pyautogui.leftClick()
         continue
